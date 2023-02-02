0a4d8f0262b23de4104e517351915f6ad24477d4
Giovanni BATY <giogio85.gb@gmail.com>
2023-02-02 14:44:37 +0100

    feat: boostrap ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { C2Component } from './c2/c2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    C2Component
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/c2/c2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {NgbAccordionModule} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'tc-c2',
+  templateUrl: './c2.component.html',
+  imports: [NgbAccordionModule],
+  standalone: true,
+  styleUrls: ['./c2.component.scss']
+})
+export class C2Component {
+
+}
