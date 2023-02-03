b06dbc3182d4d38d8f4b692a2e282518d758b35b
Giovanni BATY <giogio85.gb@gmail.com>
2023-02-03 11:02:10 +0100

    feat: colleague List +css

---
+++ src/app/shared/list-colleague/list-colleague.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input} from '@angular/core';
+import {Colleague} from "../models/Colleague";
+
+@Component({
+  selector: 'tc-list-colleague',
+  templateUrl: './list-colleague.component.html',
+  styleUrls: ['./list-colleague.component.scss']
+})
+export class ListColleagueComponent {
+
+
+  listCo:Colleague[]=[
+    {
+      photo: "https://picsum.photos/200/250",
+      pseudo: "Gio",
+      score: 0,
+    },
+    {
+      photo: "https://picsum.photos/200/251",
+      pseudo: "moi",
+      score: 0,
+    },
+    {
+      photo: "https://picsum.photos/200/252",
+      pseudo: "vin",
+      score: 0,
+    },
+    {
+      photo: "https://picsum.photos/200/253",
+      pseudo: "lolo",
+      score: 0,
+    },
+    {
+      photo: "https://picsum.photos/200/254",
+      pseudo: "toi",
+      score: 0,
+    },
+    {
+      photo: "https://picsum.photos/200/255",
+      pseudo: "paul",
+      score: 0,
+    },
+    {
+      photo: "https://picsum.photos/200/256",
+      pseudo: "able",
+      score: 0,
+    }
+  ]
+
+}
